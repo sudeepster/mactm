4936f3b9fcede7bfb9b109050b7e64bd3eea94c8
spradhan <spradhan@cs.ucla.edu>
2011-05-08 19:03:57 -0700

    added toggle button and spinner to the app

--- android/mactm-activity/gen/edu/ucla/cs/m117/grad/R.java
+++ android/mactm-activity/gen/edu/ucla/cs/m117/grad/R.java
@@ -8,6 +8,9 @@
 package edu.ucla.cs.m117.grad;
 
 public final class R {
+    public static final class array {
+        public static final int views=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class color {
@@ -16,11 +19,20 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int pedometer_button=0x7f070000;
+        public static final int pedometer_label=0x7f070001;
+        public static final int view_selector=0x7f070002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int button_off_text=0x7f050004;
+        public static final int button_on_text=0x7f050003;
         public static final int explanation=0x7f050001;
+        public static final int pedometer_label=0x7f050002;
+        public static final int view_prompt=0x7f050005;
     }
 }
--- android/mactm-activity/src/edu/ucla/cs/m117/grad/MyActivity.java
+++ android/mactm-activity/src/edu/ucla/cs/m117/grad/MyActivity.java
@@ -2,6 +2,10 @@ package edu.ucla.cs.m117.grad;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import java.util.Date;
 
 public class MyActivity extends Activity
 {
@@ -11,5 +15,33 @@ public class MyActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.pedometer_button);
+        final TextView pedometerStateLabel = (TextView) findViewById(R.id.pedometer_label);
+
+        if (toggleButton.isChecked()) {
+            pedometerStateLabel.setText("Pedometer is On.");
+        } else {
+            pedometerStateLabel.setText("Pedometer is Off.");
+        }
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (toggleButton.isChecked()) {
+                    pedometerStateLabel.setText("Pedometer is On.");
+                } else {
+                    pedometerStateLabel.setText("Pedometer is Off.");
+                }
+            }
+        });
+
+    Spinner spinner = (Spinner) findViewById(R.id.view_selector);
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+            this, R.array.views, android.R.layout.simple_spinner_item);
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    spinner.setAdapter(adapter);
+
+
+        //setContentView(R.layout.main);
     }
 }
