60421c8535711de58e59872413a5baba9ac44f5c
spradhan <spradhan@cs.ucla.edu>
2011-05-30 16:08:59 -0700

    graphs added

--- android/mactm-activity/src/edu/ucla/cs/m117/grad/MyActivity.java
+++ android/mactm-activity/src/edu/ucla/cs/m117/grad/MyActivity.java
@@ -3,7 +3,9 @@ package edu.ucla.cs.m117.grad;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
+import edu.ucla.cs.m117.grad.graphs.GraphView;
 
 import java.util.Date;
 
@@ -75,6 +77,13 @@ public class MyActivity extends Activity
                     //To change body of implemented methods use File | Settings | File Templates.
                 }
             });
+
+            float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f };
+            String[] verlabels = new String[] { "great", "ok", "bad" };
+            String[] horlabels = new String[] { "today", "tomorrow", "next week", "next month" };
+            GraphView graphView = new GraphView(this, values, "GraphViewDemo",horlabels, verlabels, GraphView.LINE);
+            addContentView(graphView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                    150));
         } else {
             final LinearLayout pedoOnLayout = (LinearLayout)findViewById(R.id.pedoOn);
             final LinearLayout pedoOffLayout = (LinearLayout)findViewById(R.id.pedoOff);
